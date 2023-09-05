1d49fd1b2f94a8b8243a5d774afdd6b4e4839b54
OtavioSantanna <otavio.santana085@gmail.com>
2023-09-05 12:19:17 -0300

    update

--- php/conecta.php
+++ php/conecta.php
@@ -1,13 +1,14 @@
 <?php
   // DEFININDO O FUSO HORÁRIO:
   date_default_timezone_set('America/Sao_Paulo');
-
-  // CONEXÃO COM A BASE DE DADOS: verifique sempre o dbname
-  try{
-    $pdo = new PDO("mysql:dbname=bombeiros-jhom;host=localhost;charset=utf8","root","");
+  
+  // Conexão com o banco de dados utilzando PDO para consultas preparadas:
+  try
+  {
+      $pdo = new PDO("mysql:dbname=bombeiros_johm;host=localhost;charset=utf8","root","");
   }
   catch(PDOException $erro)
   {
-    echo("ERRO NA CONEXÃO: <br>".$erro->getMessage());
+      echo("ERRO NA CONEXÃO: <br>".$erro->getMessage());
   }
 ?>
--- php/inserts/inserir_usuario.php
+++ php/inserts/inserir_usuario.php
@@ -1,35 +1,35 @@
 <?php
     include("../conecta.php");
     
-    $matricula = ["matricula"];
-    $nome = ["nome"];
-    $senha = ["senha"];
-    $cep = ["cep"];
-    $email = ["email"];
-    $telefone = ["telefone"];
-    $cargo = ["cargo"];
-    $admin = ["admin"];
+    $matricula = $_POST["matricula"];
+    $nome = $_POST["nome"];
+    $senha = $_POST["senha"];
+    $cep = $_POST["cep"];
+    $email = $_POST["email"];
+    $telefone = $_POST["telefone"];
+    $cargo = $_POST["cargo"];
+    $idade = $_POST["idade"];
+    $num_casa = $_POST["num_casa"];
 
     $sql = $pdo->prepare("INSERT INTO usuarios
-    (matricula, nome, senha) VALUES (?,?,?,?);");
-
-$sql->bindParam(1, $matricula);
-$sql->bindParam(2, $nome);
-$sql->bindParam(3, $senha);
-$sql->bindParam(4, $admin);
-
-$sql->execute();
-
-    $executar = $sql->execute();
-
-    $sql = $pdo->prepare("INSERT INTO info_usuario (?,?,?,?,?,?)");
+    (matricula, nome, senha) VALUES (?,?,?);");
 
     $sql->bindParam(1, $matricula);
     $sql->bindParam(2, $nome);
-    $sql->bindParam(3, $email);
-    $sql->bindParam(4, $telefone);
-    $sql->bindParam(5, $cep);
-    $sql->bindParam(6, $cargo);    
+    $sql->bindParam(3, $senha);
+    
+    $executar = $sql->execute();
+
+    $sql = $pdo->prepare("INSERT INTO info_usuarios (matricula, nome, idade, cargo, email, cep, num_casa, telefone) values (?,?,?,?,?,?,?,?)");
+
+    $sql->bindParam(1, $matricula);
+    $sql->bindParam(2, $nome);
+    $sql->bindParam(3, $idade);
+    $sql->bindParam(4, $cargo);
+    $sql->bindParam(5, $email);
+    $sql->bindParam(6, $cep);
+    $sql->bindParam(7, $num_casa);
+    $sql->bindParam(8, $telefone);    
 
     $executar = $sql->execute();
 
