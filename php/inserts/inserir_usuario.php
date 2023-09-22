e65db0d01f6789113fe228046a8a28b7e661cf57
OtavioSantanna <otavio.santana085@gmail.com>
2023-09-22 20:08:22 -0300

    update

--- php/inserts/inserir_usuario.php
+++ php/inserts/inserir_usuario.php
@@ -1,51 +1,56 @@
 <?php
-    include("../conecta.php");
-    
-    $matricula = $_POST["matricula"];
-    $nome = $_POST["nome"];
-    $senha = $_POST["senha"];
-    $cep = $_POST["cep"];
-    $email = $_POST["email"];
-    $telefone = $_POST["telefone"];
-    $cargo = $_POST["cargo"];
-    $idade = $_POST["idade"];
-    $num_casa = $_POST["num_casa"];
+  include("../conecta.php");
+  
+  $matricula = $_POST["matricula"];
+  $nome = $_POST["nome"];
+  $senha = $_POST["senha"];
+  $cep = $_POST["cep"];
+  $email = $_POST["email"];
+  $telefone = $_POST["telefone"];
+  $cargo = $_POST["cargo"];
+  $idade = $_POST["idade"];
+  $num_casa = $_POST["num_casa"];
 
-    $user_info = []; //Variável para guardar os dados acima no form
+  $user_info = []; //Variável para guardar os dados acima no form
 
-    // O trecho abaixo pode estar dentro de um WHILE para guardar a resposta de um SELECT por exemplo
-    // O trecho abaixo pode estar dentro de um WHILE para guardar a resposta de um SELECT por exemplo
-    $resposta = 
-        [
-            "matricula" => $matricula,
-            "nome" => $nome
-        ];
-    array_push($user_info, $resposta);
-    //Até aqui ficaria dentro do WHILE
-    
+  // O trecho abaixo pode estar dentro de um WHILE para guardar a resposta de um SELECT por exemplo
+  // O trecho abaixo pode estar dentro de um WHILE para guardar a resposta de um SELECT por exemplo
+  $resposta = 
+  [
+    "matricula" => $matricula,
+    "nome" => $nome
+  ];
+  array_push($user_info, $resposta);
+  //Até aqui ficaria dentro do WHILE
+  
+  try {
+  $sql = $pdo->prepare("INSERT into usuarios (matricula, nome, senha) VALUES (?, ?, ?);");
 
-    $sql = $pdo->prepare("INSERT into usuarios (matricula, nome, senha) VALUES (?, ?, ?);");
+  $sql->bindParam(1, $matricula);
+  $sql->bindParam(2, $nome);
+  $sql->bindParam(3, $senha);
+  
+  $executar = $sql->execute();
 
-    $sql->bindParam(1, $matricula);
-    $sql->bindParam(2, $nome);
-    $sql->bindParam(3, $senha);
-    
-    $executar = $sql->execute();
+  $sql = $pdo->prepare("INSERT INTO info_usuarios (matricula, nome, idade, cargo, email, cep, num_casa, telefone) values (?,?,?,?,?,?,?,?)");
 
-    $sql = $pdo->prepare("INSERT INTO info_usuarios (matricula, nome, idade, cargo, email, cep, num_casa, telefone) values (?,?,?,?,?,?,?,?)");
+  $sql->bindParam(1, $matricula);
+  $sql->bindParam(2, $nome);
+  $sql->bindParam(3, $idade);
+  $sql->bindParam(4, $cargo);
+  $sql->bindParam(5, $email);
+  $sql->bindParam(6, $cep);
+  $sql->bindParam(7, $num_casa);
+  $sql->bindParam(8, $telefone);    
 
-    $sql->bindParam(1, $matricula);
-    $sql->bindParam(2, $nome);
-    $sql->bindParam(3, $idade);
-    $sql->bindParam(4, $cargo);
-    $sql->bindParam(5, $email);
-    $sql->bindParam(6, $cep);
-    $sql->bindParam(7, $num_casa);
-    $sql->bindParam(8, $telefone);    
-
-    $executar = $sql->execute();
-
-    /*header('Location: ../../pages/central/inserir_usuario.html');
-    ob_end_flush(); // Encerra o buffer de saída e envia a saída para o navegador*/
-
+  $executar = $sql->execute();
+  
+  $json_texto = json_encode(["info" => $user_info]);
+  echo($json_texto);  // Será retornado para dentro do "success" do arquivo index.html
+  }
+  catch (PDOException $e){
+    echo "Erro no banco de dados: " . $e->getMessage();
+  }
+  /*header('Location: ../../pages/central/inserir_usuario.html');
+  ob_end_flush(); // Encerra o buffer de saída e envia a saída para o navegador*/
 ?>
