a608c480c5053a879be655f3f8cc0039bad9cc2c
joaofilipigtissi <joaofilipigtissi@gmail.com>
2023-10-17 12:11:58 -0300

    dasd

---
+++ pages/inserir_usuario.php
@@ -0,0 +1,90 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="../css/cadastro.css">
+  <title>Inserir usuário</title>
+</head>
+<script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+<body>
+    <div class="header">
+            <div class="logo">
+                <img src="../public/logo.png" width="100px" height="100px">
+                <div class="column">
+                    <div class="titulo">Núcleo de Operações Aéreas e Resgate</div>
+                    <div class="sub">Assoc. de Serviços Sociais</div>
+                    <div class="sub2">Voluntários</div>
+                </div>
+            </div>
+        </div>
+    <div class="container">
+        <div class="titulo">CADASTRAR USUÁRIO</div>
+        <script>
+            function Cadastrar(){
+            //A variável "dados" conterá todos os campos do <form id="form1">
+            var dados = $('#form1').serialize(); // TODOS os campos do form devem ter name
+
+            $.ajax({
+                type: "POST",
+                url: "../../php/inserts/inserir_usuario.php",
+                data: dados,
+                dataType: 'json',
+
+                success: function(meu_json)
+                {
+                var nome = meu_json;
+                console.log(nome);
+                }
+            })
+            }
+        </script>
+        <form method="post" action="../../php/inserts/inserir_usuario.php" id="form1">
+            <div class="opcao">
+                <div class="meio">
+                    Matricula: 
+                    <div class="caixa"><input type="number" id="matricula" name="matricula"></div>
+                    Nome:
+                    <div class="caixa"><input type="text" id="nome" name="nome"></div>
+                    Senha:
+                    <div class="caixa"><input type="text" id="senha" name="senha"></div>
+                    CPF:
+                    <div class="caixa"><input type="number" id="cpf" name="cpf"></div>
+                    CEP:
+                    <div class="caixa"><input type="number" id="cep" name="cep"></div>
+                </div>
+                <div class="meio">
+                    Numero da Casa:
+                    <div class="caixa"><input type="number" id="num_casa" name="num_casa"></div>
+                    Email:
+                    <div class="caixa"><input type="text" id="email" name="email"></div>
+                    Idade:
+                    <div class="caixa"><input type="number" id="idade" name="idade"></div>
+                    Telefone:
+                    <div class="caixa"><input type="number" id="telefone" name="telefone"></div>
+                    Cargo:
+                    <select name="cargo" id="cargo">
+                    <?php
+                    include("../../php/conecta.php");
+                    $comando = $pdo->prepare("SELECT * from cargo");
+                    $resultado = $comando->execute();
+
+                    while($linhas = $comando->fetch()) {
+                    $id_cargo = $linhas["id_cargo"];
+                    $cargo = $linhas["cargo"];
+
+                    echo("
+                    <option value='$id_cargo'>$cargo</option>
+                    ");
+                    }
+                    ?>
+                    </select>
+                </div>
+            </div>
+            <br>
+            <input type="button" name="enviar" value="Enviar" onclick="Cadastrar();" />
+        </form>
+    </div>
+</body>
+</html>
