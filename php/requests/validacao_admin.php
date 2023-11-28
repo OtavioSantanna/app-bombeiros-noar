836288fa85a0a3f88c2b90e34c220794408bd053
OtavioSantanna <otavio.santana085@gmail.com>
2023-11-27 21:51:40 -0300

    update

---
+++ php/requests/validacao_admin.php
@@ -0,0 +1,34 @@
+<?php
+    //Inicia a seção
+    session_start();
+    include("../conecta.php");
+
+    $matricula = $_POST["matricula"];
+    $senha = md5($_POST["senha"]);
+
+    $validar = $pdo->prepare("SELECT * FROM usuario WHERE matricula = :matricula AND senha = :senha AND adm = sim");
+    $validar->bindParam(':matricula', $matricula);
+    $validar->bindParam(':senha', $senha);
+    $validar->execute();
+
+    $pegar_id = $validar->fetch(PDO::FETCH_ASSOC);
+    $id_socorrista = $pegar_id['id_socorrista'];
+
+    if($validar->rowCount() == 0)
+    {
+        echo
+        ("
+            <script type=text/javascript>
+                alert('matricula ou senha incorretos')
+                window.location = '../../pages/central/login.html'     
+            </script>
+        ");
+    }
+    else
+    {
+        $_SESSION["usuario"] = $usuario_info["usuario"];
+        $_SESSION["matricula"] = $matricula;
+        $_SESSION["id_socorrista"] = $id_socorrista;
+        header('Location: ../../pages/central/central.php');
+    }
+?>
