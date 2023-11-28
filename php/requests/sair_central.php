dd126ee98a56a32bb9c42224d559d04fea41e463
OtavioSantanna <otavio.santana085@gmail.com>
2023-11-27 22:05:00 -0300

    update

---
+++ php/requests/sair_central.php
@@ -0,0 +1,4 @@
+<?php
+    session_destroy();
+    header('Location: ../../pages/central/login.html');
+?>
---
+++ php/requests/sair_medico.php
@@ -0,0 +1,4 @@
+<?php
+    session_destroy();
+    header('Location: ../../pages/medico/login.html');
+?>
